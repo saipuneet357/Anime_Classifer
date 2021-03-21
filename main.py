bd440308748354631214a64c02e265c4ff9f667b
puneet <saipuneet357@gmail.com>
2021-03-21 11:44:31 +0530

    removed overfitting and increased accuracy to 50% for 40 epochs

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
 from keras import backend as K
-from keras.layers.core import Dropout
+#from keras.layers.core import Dropout
+from keras import regularizers
+
 
 
 class anime_model:
@@ -22,25 +24,24 @@ class anime_model:
 			
 		#First Layer
 		model.add(Conv2D(32, 5, padding='same', input_shape=inputShape, 		    
-				activation=activation))
+				activation=activation, use_bias=True))
 		model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-		model.add(Dropout(0.5))
 		
 		#Second Layer
 		model.add(Conv2D(64, 5, padding='same', input_shape=inputShape, 		    
-				activation=activation))
+				activation=activation, use_bias=True))
 		model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-		model.add(Dropout(0.5))		
+		
 		
 		#Flatten and FC Layer
 		model.add(Flatten())
-		model.add(Dense(1000, activation=activation))
+		model.add(Dense(1000, activation=activation, use_bias=True))
 		
 		#Dropout Layer
-		model.add(Dropout(0.5))
+		#model.add(Dropout(0.5))
 		
 		#Output Layer
-		model.add(Dense(labels, activation='softmax'))
+		model.add(Dense(labels, activation='softmax', use_bias=True))
 		
 		if weightsPath is not None:
 			model.load_weights(weightsPath)
--- train.py
+++ train.py
@@ -7,7 +7,9 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
 from keras import backend as K
 from keras.optimizers import SGD
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
+from keras.preprocessing.image import ImageDataGenerator
+
 
 def list_all_files(path):
 	files = []
@@ -47,8 +49,8 @@ for filePath in filePaths:
 	
 	data.append(image)
 	labels.append(filePath.split(os.path.sep)[-2])
-	
-	
+
+
 
 trainX, testX, trainY, testY = train_test_split(data, labels, test_size=0.25, random_state=42)
 
@@ -69,15 +71,31 @@ if K.image_data_format() == 'channels_first':
 else:
 	trainX = trainX.reshape((trainX.shape[0], 120, 120, 1))
 	testX = testX.reshape((testX.shape[0], 120, 120, 1))
+
+#Data Augmentation
+#aug = ImageDataGenerator()
+print('[INFO] Performing on the fly augmentation.......')
+aug = ImageDataGenerator(
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	horizontal_flip=True,
+	fill_mode='nearest'
+)
+
+INIT_LR = 0.01
+EPOCHS = 70
 	
-opt = SGD(lr=0.01)
+opt = SGD(lr=INIT_LR, momentum=0.9, decay=INIT_LR / EPOCHS)
 
 model = nn.build(rows=120, columns=120, channels=1, labels=len(lb.classes_), weightsPath=args['load'])
-model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])q
 
 if args['load'] == None:
 	print('[INFO] training..........')
-	result = model.fit(trainX, trainY, batch_size=32, epochs=50, verbose=1)
+	history = model.fit(x=aug.flow(trainX, trainY, batch_size=32), validation_data=(testX, testY), epochs=EPOCHS, verbose=2)
 	
 
 if args['weights'] != None:
@@ -88,14 +106,31 @@ else:
 
 
 print('[INFO] evaluating...........')
-(loss, accuracy) = model.evaluate(testX, testY, batch_size=128, verbose=1)
-print('[INFO] accuracy: {}.........'.format(accuracy*120))
+(loss, accuracy) = model.evaluate(testX, testY, batch_size=128, verbose=2)
+print('[INFO] accuracy: {}.........'.format(accuracy*100))
 
-#plot results
-pyplot.plot(result.history['accuracy'], label='train')
-pyplot.plot(result.history['accuracy'], label='test')
-pyplot.legend()
-pyplot.show()
+#plot accuracy results
+plt.plot(history.history['accuracy'], label='train')
+plt.plot(history.history['val_accuracy'], label='test')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.title('model accuracy')
+plt.legend()
+plt.savefig('accuracy.png')
+plt.show()
+
+#plot loss results
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.title('model loss')
+plt.legend()
+plt.savefig('loss.png')
+plt.show()
+
+
+
 
 
 
