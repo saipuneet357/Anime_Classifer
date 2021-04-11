0238f7bb83ea35ad81df191299f839a174ab4d7a
puneet <saipuneet357@gmail.com>
2021-04-11 13:32:25 +0530

    add text to remove cache

--- static/style.js
+++ static/style.js
@@ -3,4 +3,9 @@ function removeUpload() {
   $('.file-upload-input').replaceWith($('.file-upload-input').clone());
   $('.file-upload-content').hide();
   $('.image-upload-wrap').show();
+  location.reload(true);
+  window.location = window.location.href+'?eraseCache=true';
 }
+
+
+
